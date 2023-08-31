b031b678748d7fee5b032240c163d5f5b710a388
Pedro Burini <pulisses10@gmail.com>
2023-08-31 14:39:54 -0300

    qlineedit and display

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 from main_window import MainWindow
+from display import Display
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from variables import WINDOW_ICON_PATH
@@ -14,6 +15,10 @@ if __name__ == '__main__':
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+
+    # display
+    display = Display()
+    window.addToVLayout(display)
 
     # execute all
     window.adjustFixedSize()
