902538d0fc4b8dbf51b7ee23ec86b83e0270f2b4
Pedro Burini <pulisses10@gmail.com>
2023-08-31 14:00:54 -0300

    adding widgets in vlayout

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import sys
 
 from main_window import MainWindow
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication
+from variables import WINDOW_ICON_PATH
 
 if __name__ == '__main__':
+    # create application
     app = QApplication(sys.argv)
     window = MainWindow()
 
-    label1 = QLabel('O meu texto')
-    label1.setStyleSheet('font-size: 150px;')
-    window.v_layout.addWidget(label1)
-    window.adjustFixedSize()
+    # define icon
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
 
+    # execute all
+    window.adjustFixedSize()
     window.show()
     app.exec()
